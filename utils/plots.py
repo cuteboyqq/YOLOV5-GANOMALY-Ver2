5172d65b61ddd28330a3c938aa8ea4a241ed517c
cuteboyqq <58428559+cuteboyqq@users.noreply.github.com>
2022-12-13 16:36:59 +0800

    Add files via upload

--- utils/plots.py
+++ utils/plots.py
@@ -560,31 +560,32 @@ def profile_idetection(start=0, stop=0, labels=(), save_dir=''):
 '''
     Alister add 2022-10-13
 '''
-def get_crop_image(xyxy, im, BGR=False):
+def get_crop_image(xyxy, im, BGR=False, gain=1.02, pad=10):
     # Save image crop as {file} with crop size multiple {gain} and {pad} pixels. Save and/or return crop
     xyxy = torch.tensor(xyxy).view(-1, 4)
-    b = xyxy2xywh(xyxy)  # boxes
+    #b = xyxy2xywh(xyxy)  # boxes
     #if square:
         #b[:, 2:] = b[:, 2:].max(1)[0].unsqueeze(1)  # attempt rectangle to square
-    b[:, 2:] = b[:, 2:]  # box wh * gain + pad
-    xyxy = xywh2xyxy(b).long()
-    clip_boxes(xyxy, im.shape)
+    #b[:, 2:] = b[:, 2:]  # box wh * gain + pad
+    #xyxy = xywh2xyxy(b).long()
+    clip_boxes(xyxy.long(), im.shape)
     crop = im[int(xyxy[0, 1]):int(xyxy[0, 3]), int(xyxy[0, 0]):int(xyxy[0, 2]), ::(1 if BGR else -1)]
     return crop        
 
-def save_one_box(xyxy, im, file=Path('im.jpg'), gain=1.02, pad=10, square=False, BGR=False, save=True):
+def save_one_box(xyxy, im, file=Path('im.jpg'), gain=1.00, pad=0, square=False, BGR=False, save=True):
     # Save image crop as {file} with crop size multiple {gain} and {pad} pixels. Save and/or return crop
     xyxy = torch.tensor(xyxy).view(-1, 4)
-    b = xyxy2xywh(xyxy)  # boxes
-    if square:
-        b[:, 2:] = b[:, 2:].max(1)[0].unsqueeze(1)  # attempt rectangle to square
-    b[:, 2:] = b[:, 2:] * gain + pad  # box wh * gain + pad
-    xyxy = xywh2xyxy(b).long()
-    clip_boxes(xyxy, im.shape)
+    #b = xyxy2xywh(xyxy)  # boxes
+    #if square:
+        #b[:, 2:] = b[:, 2:].max(1)[0].unsqueeze(1)  # attempt rectangle to square
+    #b[:, 2:] = b[:, 2:]  # box wh * gain + pad
+    #xyxy = xywh2xyxy(b).long()
+    clip_boxes(xyxy.long(), im.shape)
     crop = im[int(xyxy[0, 1]):int(xyxy[0, 3]), int(xyxy[0, 0]):int(xyxy[0, 2]), ::(1 if BGR else -1)]
     if save:
         file.parent.mkdir(parents=True, exist_ok=True)  # make directory
         f = str(increment_path(file).with_suffix('.jpg'))
-        # cv2.imwrite(f, crop)  # save BGR, https://github.com/ultralytics/yolov5/issues/7007 chroma subsampling issue
-        Image.fromarray(crop[..., ::-1]).save(f, quality=95, subsampling=0)  # save RGB
+        #crop = cv2.resize(crop,(32,32))
+        #cv2.imwrite(f, crop)  # save BGR, https://github.com/ultralytics/yolov5/issues/7007 chroma subsampling issue
+        Image.fromarray(crop[..., ::-1]).save(f, quality=100, subsampling=0)  # save RGB
     return crop
